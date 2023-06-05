e1be86c2d7edc938334f836bb44b52ada0e7a420
Angel <a01253832@itesm.mx>
2023-06-05 16:13:19 -0700

    first server and client test

--- src/TestServer.py
+++ src/TestServer.py
@@ -1,9 +1,20 @@
 from opcua import Server 
+import time
 
 server = Server()
+
 root = server.get_objects_node()
-
-start = root.add_variable("ns=1;s=start","Start",False)
-finish = root.add_variable("ns=2;s=finish","Finish",False)
-
+start = root.add_variable("ns=1;s=Start", "Start", False)
+finish = root.add_variable("ns=2;s=Finish", "Finish", True)
 server.start()
+print("Servidor OPC UA iniciado")
+
+
+while True:
+    start.set_value(True)  
+    finish.set_value(False)
+    time.sleep(3) 
+    start.set_value(False) 
+    finish.set_value(True)
+    time.sleep(3) 
+
